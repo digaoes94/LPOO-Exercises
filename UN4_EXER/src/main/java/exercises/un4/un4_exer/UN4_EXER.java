b2a720c2edd9999779da1afdefb30ac99364a8a3
Rodrigo Araujo <rasch.dev@gmail.com>
2025-09-30 08:37:50 -0300

    Project created

---
+++ UN4_EXER/src/main/java/exercises/un4/un4_exer/UN4_EXER.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercises.un4.un4_exer;
+
+/**
+ *
+ * @author Usuario
+ */
+public class UN4_EXER {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
