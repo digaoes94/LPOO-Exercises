3fed0353e7d1d7ef71d4d66f4e945d37fb06a2d0
Rodrigo Araujo <rasch.dev@gmail.com>
2025-10-05 14:48:01 -0300

    UsaAutomovel partially done

--- UN4_EXER/src/main/java/exercises/un4/exer1/UsaAutomovel.java
+++ UN4_EXER/src/main/java/exercises/un4/exer1/UsaAutomovel.java
@@ -1,8 +1,54 @@
 package exercises.un4.exer1;
 
-public class UsaAutomovel {
+import exercises.un4.exer1.Automovel;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class UsaAutomovel {
+    static Scanner scan = new Scanner(System.in);
+    
     public static void UsaAutomovel(String[] args) {
+        ArrayList<Automovel> lista = new ArrayList<Automovel>();
+        int opcao = 0;
         
+        System.out.println("Bem vindo ao UsaAutomóvel!");
+        do {
+            opcao = menu(opcao);
+            
+            switch(opcao) {
+                case 1:
+                    inserirAuto(lista);
+                    break;
+                case 2:
+                    pesquisarAuto(lista);
+                    break;
+                case 3:
+                    excluirAuto(lista);
+                    break;
+                case 4:
+                    listarAutos(lista);
+                    break;
+                case 5:
+                    System.out.println("Fechando programa.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Entrada inválida. O programa será encerrado.");
+                    System.exit(0);
+            }
+        } while(true);
+    }
+    
+    private static int menu(int opcao) {
+        System.out.println("Favor selecionar procedimento:");
+        System.out.println("1. Adicionar automóvel ao sistema");
+        System.out.println("2. Pesquisar automóvel");
+        System.out.println("3. Excluir automóvel");
+        System.out.println("4. Ver todos os automóveis no sistema");
+        System.out.println("5. Fechar programa");
+        
+        opcao = scan.nextInt();
+        scan.nextLine();
+        return opcao;
     }
 }
